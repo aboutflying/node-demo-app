d90a0f190fcab878e8b541b6794d6de5196ee91a
Bill Reynolds <wdreynolds@gmail.com>
2019-04-19 10:03:48 -0400

    chown files to jenkins user

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ cd /root
 apt-get install -y npm
 git-buildpackage --git-ignore-new --git-ignore-branch
 cp ../*.deb .
+status=$?
+find . -not -uid $(stat -c "%u" .) -exec chown --reference=. {} \;
+exit $status # exit with the saved status value
