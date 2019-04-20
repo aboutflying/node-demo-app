380cf1f130406efc85d6950de7ecf9394206a830
Bill Reynolds <wdreynolds@gmail.com>
2019-04-19 21:31:13 -0400

    Hello world message returns hostname

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
+var exec = require('child_process').exec;
+var child;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!!!');
+  child = exec("hostname",
+  function (error, stdout, stderr) {
+     var hostname = JSON.stringify(stdout).replace(/"/g, "");
+     res.send('Hello from ' + hostname + '!');
+  });
 });
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening on port %s', port);
 });
