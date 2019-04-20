9a2ac488701450b539ee4f8f5a701e7e738b43c9
Bill Reynolds <wdreynolds@gmail.com>
2019-04-20 10:19:52 -0400

    fix newline escape

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
 var exec = require('child_process').exec;
-var child;
 
 app.get('/', function (req, res) {
-  child = exec("hostname",
+  var child = exec("hostname",
   function (error, stdout, stderr) {
-     var hostname = JSON.stringify(stdout).replace(/"/g, "").replace(/\n/g, "");
+     var hostname = JSON.stringify(stdout).replace(/"/g, "").replace(/\\n/g, "");
      res.send('Hello from ' + hostname + '!');
   });
 });
